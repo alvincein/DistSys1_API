792585c3933b0098a0c18bae20876f0f9a92a16d
alvincein <tpapakons@gmail.com>
2019-11-19 14:53:29 +0200

    Fix CORS problem

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 8000;
 const path = require('path');
+const cors = require('cors');
     
     var bodyParser = require("body-parser");
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
+
+    // Enable cors requests
+    app.use(cors());
 
     // Set static folder
     app.use(express.static(path.join(__dirname, 'public')));
@@ -23,7 +27,7 @@ const path = require('path');
     // Members api routes
     app.use('/api/members', require('./routes/api/members'));
     // Actions' routes
-    app.use('/api' , require('./routes/api/actions'))
+    app.use('/api', require('./routes/api/actions'));
 
     // Default response for any other request
     app.use(function(req, res){
